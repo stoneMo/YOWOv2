ef42b44371d59d45a79195f5497f06c40af19da6
ShentongMo <shentonm@andrew.cmu.edu>
2020-10-15 14:41:54 +0800

    add run_frame_mAP_jhmdb.sh

--- run_frame_mAP_jhmdb.sh
+++ run_frame_mAP_jhmdb.sh
@@ -1,4 +1,8 @@
-python ./evaluation/Object-Detection-Metrics/pascalvoc.py --gtfolder groundtruths_jhmdb \
-		--detfolder ../../jhmdb_detections/detections_1
-
-
+#!/bin/bash
+NumEpoch=25
+for ((i=1; i<=NumEpoch; i++))
+do
+    echo $i 
+    python ./evaluation/Object-Detection-Metrics/pascalvoc.py --gtfolder groundtruths_jhmdb \
+                --detfolder ../../jhmdb_detections/detections_$i
+done
