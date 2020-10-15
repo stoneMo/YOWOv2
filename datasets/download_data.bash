ef92ec133bb7926d48b24135e541aead5e8f84c6
ShentongMo <shentonm@andrew.cmu.edu>
2020-10-15 18:09:31 +0800

    modify download_data.bash

--- datasets/download_data.bash
+++ datasets/download_data.bash
@@ -7,5 +7,5 @@ curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
 
 # unzip 
-unzip -d ./datasets/jhmdb jhmdb.zip
+unzip -d ./datasets/ jhmdb.zip
 
