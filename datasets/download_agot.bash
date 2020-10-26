9c0c4378fd53ef4f7af287ad7a09180d0aff6e98
ellenxtan <xiaoqingtan712@gmail.com>
2020-10-26 16:44:32 +0000

    update download_agot.bash

---
+++ datasets/download_agot.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# download jhmdb from Google-drive
+fileid="1ZqFneqlFuHiqTSk2npDZfsv28KBd7XjJ"
+filename="agot-24.zip"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+
+# unzip 
+unzip -d ./datasets/ agot.zip
