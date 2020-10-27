5ec0386ae88fdc8619a0c94082c3aa3c6edd0675
XQ <38412452+ellenxtan@users.noreply.github.com>
2020-10-26 21:28:49 -0400

    Update download_agot.bash

--- datasets/download_agot.bash
+++ datasets/download_agot.bash
@@ -7,4 +7,4 @@ curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
 
 # unzip 
-unzip -d ./datasets/ agot.zip
+unzip -d ./datasets/ agot-24.zip
